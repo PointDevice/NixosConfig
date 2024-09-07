661905f1ddf6cf0d35b7eeadb67aa949750535fd
PointDevice <rockit1225@gmail.com>
2024-09-07 04:31:32 -0600

    update spicetify

--- nixos/home-manager/spicetify.nix
+++ nixos/home-manager/spicetify.nix
@@ -18,41 +18,41 @@ in
 
   # configure spicetify :)
   programs.spicetify =
-    {
-      enable = true;
-      theme = spicePkgs.themes.Dribbblish;
-      colorScheme = "custom"; 
-      #catppuccin-macchiato";
+  {
+    enable = true;
+    theme = spicePkgs.themes.Dribbblish;
+    colorScheme = "custom"; 
+    #catppuccin-macchiato";
 
-      enabledExtensions = with spicePkgs.extensions; [
-        fullAppDisplay
-        shuffle # shuffle+ (special characters are sanitized out of ext names)
-        hidePodcasts
-        (let
-          dummy = builtins.toFile "dummy.js" "";
-        in { 
-          src = builtins.dirOf dummy;
-          filename = builtins.baseNameOf dummy;
-          experimentalFeatures = true;
-        })
-      ];
-      customColorScheme = {
-        text               = "CAD3F5";
-        subtext            = "CAD3F5";
-        sidebar-text       = "cad3f5";
-        main               = "24273A";
-        sidebar            = "1E2030";
-        player             = "181926";
-        card               = "363a4f";
-        shadow             = "1E2030";
-        selected-row       = "939ab7";
-        button             = "8bd5ca";#"8087A2";
-        button-active      = "8bd5ca";#"939AB7";
-        button-disabled    = "6E738D";
-        tab-active         = "363A4F";
-        notification       = "363A4F";
-        notification-error = "ED8796";
-        misc               = "494d64";
-      };
+    enabledExtensions = with spicePkgs.extensions; [
+      fullAppDisplay
+      shuffle # shuffle+ (special characters are sanitized out of ext names)
+      hidePodcasts
+      (let
+        dummy = builtins.toFile "dummy.js" "";
+      in { 
+        src = builtins.dirOf dummy;
+        filename = builtins.baseNameOf dummy;
+        experimentalFeatures = true;
+      })
+    ];
+    customColorScheme = {
+      text               = "CAD3F5";
+      subtext            = "CAD3F5";
+      sidebar-text       = "cad3f5";
+      main               = "24273A";
+      sidebar            = "1E2030";
+      player             = "181926";
+      card               = "363a4f";
+      shadow             = "1E2030";
+      selected-row       = "939ab7";
+      button             = "8bd5ca";#"8087A2";
+      button-active      = "8bd5ca";#"939AB7";
+      button-disabled    = "6E738D";
+      tab-active         = "363A4F";
+      notification       = "363A4F";
+      notification-error = "ED8796";
+      misc               = "494d64";
     };
+  };
 } 
